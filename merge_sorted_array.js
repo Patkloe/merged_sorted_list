8a59d74ffcde8fece48b1f5652fc5bc445000925
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-11-19 22:33:12 -0600

    Update merge_sorted_array.js

--- merge_sorted_array.js
+++ merge_sorted_array.js
@@ -22,7 +22,7 @@ function merge_tab(res,p1,p2){
    if(tab1[p1] >= tab2[p2]){
      res[tab2[p2]] = tab2[p2];
      p2 = p2 + 1;
-    if(p2 < size1)
+    if(p2 < size2)
       merge_tab(res,p1,p2);
      else
       console.log("out of bound");
